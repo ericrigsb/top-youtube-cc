3127b854267fdf86d996daf231b992f16901714b
Eric <eric@rigsb.net>
2019-08-10 12:42:01 -0400

    Working on downloading video.

--- top-youtube-cc.py
+++ top-youtube-cc.py
@@ -7,10 +7,16 @@ import re
 query_string = urllib.parse.urlencode({"search_query" : input('Search YouTube for: ')})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-print("http://www.youtube.com/watch?v=" + search_results[0])
+url = "http://www.youtube.com/watch?v=" + search_results[0]
+print(url)
 
 from pytube import YouTube
+import os
 
-yt = YouTube("http://www.youtube.com/watch?v=" + search_results[0])
-yt = yt.get('mp4', '720p')
-yt.download('./')
+yt = YouTube(url)
+
+print(yt.streams.filter(subtype='mp4', progressive=True).first())
+yt.streams.filter(subtype='mp4', progressive=True).first().download()
+#yt.streams.first().download()
+
+#YouTube("http://www.youtube.com/watch?v=" + search_results[0]).streams.first().download('~/Downloads/')
