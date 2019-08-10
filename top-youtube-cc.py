cec643114bb0616e3ab2f77f3bf8f924e37eb8ee
Eric <eric@rigsb.net>
2019-08-10 15:12:50 -0400

    Added comments, filter results, remove duplicate results.

--- top-youtube-cc.py
+++ top-youtube-cc.py
@@ -3,21 +3,24 @@
 import urllib.request
 import urllib.parse
 import re
-
-# Search YouTube for a video
-
-query_string = urllib.parse.urlencode({"search_query" : input('Search YouTube for: ')})
-html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-url = "http://www.youtube.com/watch?v=" + search_results[0]
-print(url)
-
 from pytube import YouTube
 
-# Grab information regarding the video and download the progressive stream, mp4 version
+# Search YouTube for videos filtered by CC license and view count
 
-yt = YouTube(url)
+query_string = urllib.parse.urlencode({"search_query" : input('Search YouTube for: ')})
+html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string + "&sp=CAMSAjAB")
+search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
 
-print(yt.streams.filter(subtype='mp4', progressive=True).first())
-print(yt.title)
-yt.streams.filter(subtype='mp4', progressive=True).first().download()
+# Remove duplicates and limit to first three results
+
+search_results = list(dict.fromkeys(search_results))[:3]
+
+# Grab information regarding the videos and download the progressive stream, mp4 versions
+
+for result in search_results:
+    url = "http://www.youtube.com/watch?v=" + result
+    yt = YouTube(url)
+    print(yt.streams.filter(subtype='mp4', progressive=True).first())
+    print(yt.title)
+    print(yt.views)
+    # yt.streams.filter(subtype='mp4', progressive=True).first().download()
