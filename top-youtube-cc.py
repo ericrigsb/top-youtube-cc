3781763f6d136d3cd9f0a05d49c3f9498fe6088f
Eric <eric@rigsb.net>
2019-08-10 13:28:11 -0400

    Downloads are working after patching pytube issue 434.

--- top-youtube-cc.py
+++ top-youtube-cc.py
@@ -4,6 +4,8 @@ import urllib.request
 import urllib.parse
 import re
 
+# Search YouTube for a video
+
 query_string = urllib.parse.urlencode({"search_query" : input('Search YouTube for: ')})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
@@ -11,12 +13,11 @@ url = "http://www.youtube.com/watch?v=" + search_results[0]
 print(url)
 
 from pytube import YouTube
-import os
+
+# Grab information regarding the video and download the progressive stream, mp4 version
 
 yt = YouTube(url)
 
 print(yt.streams.filter(subtype='mp4', progressive=True).first())
+print(yt.title)
 yt.streams.filter(subtype='mp4', progressive=True).first().download()
-#yt.streams.first().download()
-
-#YouTube("http://www.youtube.com/watch?v=" + search_results[0]).streams.first().download('~/Downloads/')
